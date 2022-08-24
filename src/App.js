2364c6d1e2bb19236407d3cf2af110913020c830
PseudoRai <ryan.valera888@gmail.com>
2022-08-24 05:33:11 -0400

    create orders

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
 import React from "react";
-import Products from "./components/Products";
-import Filter from "./components/Filter";
-import Cart from "./components/Cart";
 import store from "./store";
 import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import AdminScreen from "./screens/AdminScreen";
+
 
 class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="grid-container">
-          <header>
-            <a href="/">React Shopping Cart</a>
-          </header>
-          <main>
-            <div className="content">
-              <div className="main">
-                <Filter></Filter>
-                <Products></Products>
-              </div>
-              <div className="sidebar">
-                <Cart />
-              </div>
-            </div>
-          </main>
-          <footer>All right is reserved.</footer>
-        </div>
+        <BrowserRouter>
+          <div className="grid-container">
+            <header>
+              {/*
+
+              When using react-router-dom in a project, you'd use "link" tag
+              instead of "a"(anchor tag), and "to" instead of "href".
+
+              <a href="/">React Shopping Cart</a> */}
+              <Link to="/">React Shopping Cart</Link>
+              <Link to="/admin">Admin</Link>
+            </header>
+            <main>
+              <Routes>
+                {/* 
+                
+                "component" in line 31 and 32 is deprecated and has been replaced by 
+                "element", as shown in line 33 and 34
+                
+                <Route path="/admin" component={AdminScreen} />
+                <Route path="/" component={HomeScreen} exact /> */}
+                <Route path="/admin" element={<AdminScreen />} />
+                <Route path="/" element={<HomeScreen />} exact />
+              </Routes>
+
+            </main>
+            <footer>All right is reserved.</footer>
+          </div>
+        </BrowserRouter>
       </Provider>
     );
   }
